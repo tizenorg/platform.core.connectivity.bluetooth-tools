1619e092526aa3cf5fcd5f9f92f0e4fa685a5623
seungku kim <seungku.kim@samsung.com>
2015-06-10 11:16:58 +0900

    3.0 specific patch: change vconftool usage
    Change-Id: I68c65f2f2de74a273b78f2b0c4c14d4aea2c0069

--- scripts/mobile/bt-reset-env.sh
+++ scripts/mobile/bt-reset-env.sh
@@ -10,13 +10,13 @@ rm -rf /opt/data/bluetooth/.bt_paired
 rm -rf /var/lib/bluetooth/*
 
 # Initialize BT vconf values
-vconftool set -tf int db/bluetooth/status "0" -g 6520
-vconftool set -tf int file/private/bt-service/flight_mode_deactivated "0" -g 6520 -i
-vconftool set -tf string memory/bluetooth/sco_headset_name "" -g 6520 -i
-vconftool set -tf int memory/bluetooth/device "0" -g 6520 -i
-vconftool set -tf int file/private/libug-setting-bluetooth-efl/visibility_time "0" -g 6520
-vconftool set -tf bool memory/private/bluetooth-share/quickpanel_clear_btn_status FALSE -g 6520 -i
-vconftool set -tf bool memory/private/bluetooth-share/opp_server_init FALSE -g 6520 -i
+vconftool set -f -t int db/bluetooth/status "0" -g 6520
+vconftool set -f -t int file/private/bt-service/flight_mode_deactivated "0" -g 6520
+vconftool set -f -t string memory/bluetooth/sco_headset_name "" -g 6520 -i
+vconftool set -f -t int memory/bluetooth/device "0" -g 6520 -i
+vconftool set -f -t int file/private/libug-setting-bluetooth-efl/visibility_time "0" -g 6520
+vconftool set -f -t bool memory/private/bluetooth-share/quickpanel_clear_btn_status FALSE -g 6520 -i
+vconftool set -f -t bool memory/private/bluetooth-share/opp_server_init FALSE -g 6520 -i
 
 # Remove BT shared memory
 list=`ipcs -m | awk '$1==0x0001000 {print $2}'`
